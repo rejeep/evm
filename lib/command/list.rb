ff322647f5fa3df8711d1aa27a4f09da6bb5f625
Johan Andersson <johan.rejeep@gmail.com>
2013-11-30 17:22:19 +0100

    Add list command.

---
+++ lib/command/list.rb
@@ -0,0 +1,22 @@
+module Evm
+  module Command
+    class List
+      def initialize(*args)
+        packages = Evm::Package.all
+        packages.each do |package|
+          if package.current?
+            STDOUT.print '* '
+          end
+
+          STDOUT.print package
+
+          if package.installed?
+            STDOUT.print ' [I]'
+          end
+
+          STDOUT.puts
+        end
+      end
+    end
+  end
+end
