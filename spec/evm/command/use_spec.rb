495baf028fd47d50009f5ad143ded7f6388a49b8
Johan Andersson <johan.rejeep@gmail.com>
2013-12-01 16:28:50 +0100

    Make sure use gets argument.

--- spec/evm/command/use_spec.rb
+++ spec/evm/command/use_spec.rb
@@ -1,15 +1,32 @@
 require 'spec_helper'
 
 describe Evm::Command::Use do
-  it 'should use package name' do
+  it 'should use package name if installed' do
     Evm::Package.stub(:find) do |package_name|
-      package_name.should == 'foo'
-
       package = double('package')
+      package.stub(:installed?).and_return(true)
       package.should_receive(:use!)
       package
     end
 
     Evm::Command::Use.new('foo')
+  end
+
+  it 'should raise exception if package is not installed' do
+    Evm::Package.stub(:find) do |package_name|
+      package = double('package')
+      package.stub(:installed?).and_return(false)
+      package
+    end
+
+    expect {
+      Evm::Command::Use.new('foo')
+    }.to raise_error('Package not installed: foo')
+  end
+
+  it 'should raise exception if no package name' do
+    expect {
+      Evm::Command::Use.new(nil)
+    }.to raise_error('The use command requires an argument')
   end
 end
