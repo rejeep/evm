de21bd01131810f6d2eaf9d0b2f8ca6a606d3751
Johan Andersson <johan.rejeep@gmail.com>
2013-12-07 14:45:02 +0100

    Fix recipe url.

--- recipes/emacs-24.1-bin.rb
+++ recipes/emacs-24.1-bin.rb
@@ -1,6 +1,5 @@
 recipe 'emacs-24.1-bin' do
-  # tar_gz 'http://s3.amazonaws.com/emacs-evm/emacs-24.1-%s.tar.gz' % platform_name
-  tar_gz 'http://localhost:8000/emacs-24.1-osx.tar.gz'
+  tar_gz 'http://s3.amazonaws.com/emacs-evm/emacs-24.1-%s.tar.gz' % platform_name
 
   install do
     copy build_path, installations_path
