caef435fc19950b53856c34ba9465d68e8b0cd45
Johan Andersson <johan.rejeep@gmail.com>
2014-10-21 10:29:23 +0200

    Add recipe for Emacs 24.4

---
+++ recipes/emacs-24.4.rb
@@ -0,0 +1,24 @@
+recipe 'emacs-24.4' do
+  tar_gz 'http://ftpmirror.gnu.org/emacs/emacs-24.4.tar.gz'
+
+  osx do
+    option '--with-ns'
+    option '--without-x'
+    option '--without-dbus'
+  end
+
+  linux do
+    option '--prefix', installation_path
+    option '--without-gif'
+  end
+
+  install do
+    configure
+    make 'bootstrap'
+    make 'install'
+
+    osx do
+      copy File.join(build_path, 'nextstep', 'Emacs.app'), installation_path
+    end
+  end
+end
