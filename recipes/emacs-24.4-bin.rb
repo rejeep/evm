65f4ab3af292f84826ad22e61ed066a67c3e8d90
Johan Andersson <johan.rejeep@gmail.com>
2014-11-01 20:22:54 +0100

    Add recipe for Emacs 24.4 bin

---
+++ recipes/emacs-24.4-bin.rb
@@ -0,0 +1,7 @@
+recipe 'emacs-24.4-bin' do
+  tar_gz 'https://github.com/rejeep/evm-bin/raw/master/emacs-24.4-%s.tar.gz' % platform_name
+
+  install do
+    copy build_path, installations_path
+  end
+end
