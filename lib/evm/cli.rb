19d48a35c81d4021ea68a010f8c44ac2d8ef8379
David Shepherd <davidshepherd7@gmail.com>
2015-06-04 23:09:48 +0100

    Add error messages when we can't write to /usr/local/evm
    Check for existance and writability of the directory, and give helpful
    error messages if the checks fail.
    
    Even though the readme says to use `chown` on the directory I think the
    correct thing to check is writablity rather than ownership (since
    advanced users could have the directory world-writable, or other strange
    things, right?).

--- lib/evm/cli.rb
+++ lib/evm/cli.rb
@@ -21,6 +21,14 @@ module Evm
         Evm.print_usage_and_exit
       end
 
+      unless File.directory?('/usr/local/evm')
+        Evm.abort "Directory '/usr/local/evm' does not exist. Did you forget to run 'sudo mkdir /usr/local/evm'?"
+      end
+
+      unless File.stat('/usr/local/evm').writable?
+        Evm.abort "You can't write to '/usr/local/evm'. Did you forget to run 'sudo chown $USER: /usr/local/evm'?"
+      end
+
       begin
         const = Evm::Command.const_get(command.capitalize)
       rescue NameError => exception
