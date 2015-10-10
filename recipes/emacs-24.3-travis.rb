44ae98e79af6b94f5ceebcb0c872465a1779aece
Johan Andersson <johan.rejeep@gmail.com>
2015-10-10 15:13:08 +0200

    Add Emacs 24.{1-4} Travis recipes

---
+++ recipes/emacs-24.3-travis.rb
@@ -0,0 +1,7 @@
+recipe 'emacs-24.3-travis' do
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.6.1/emacs-24.3-travis.tar.gz'
+
+  install do
+    copy build_path, installations_path
+  end
+end
