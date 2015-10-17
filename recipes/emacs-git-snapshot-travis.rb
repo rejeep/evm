cef7dd670aea30ea674b69ef62a5e5a9870ef5a9
Johan Andersson <johan.rejeep@gmail.com>
2015-10-17 11:00:48 +0200

    Add Emacs Git snapshot Travis recipe

---
+++ recipes/emacs-git-snapshot-travis.rb
@@ -0,0 +1,7 @@
+recipe 'emacs-git-snapshot-travis' do
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.7.0/emacs-git-snapshot-travis.tar.gz'
+
+  install do
+    copy build_path, installations_path
+  end
+end
