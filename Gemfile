0312b2e7aa9799329957c690ba4b1f8ca0f75982
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-11 07:30:22 +0100

    Setup simplecov and coveralls

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source 'https://rubygems.org'
 
 group :test do
   gem 'rspec'
+  gem 'simplecov'
+  gem 'coveralls'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,22 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'simplecov'
 
+if ENV['TRAVIS']
+  require 'coveralls'
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+else
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+end
+
+SimpleCov.start do
+  add_filter 'spec'
+end
+
+evm_lib = File.expand_path('../../lib', __FILE__)
+# Eager load the entire lib directory so that SimpleCov is able to report accurate code coverage metrics.
+at_exit { Dir["#{evm_lib}/**/*.rb"].each { |rb| require(rb) } }
+$LOAD_PATH.unshift evm_lib
 require 'evm'
 
 RSpec.configure do |config|
