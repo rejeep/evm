d525916239ae1f55f35c26bd754a7f38e1e2f9ee
Johan Andersson <johan.rejeep@gmail.com>
2016-09-23 21:37:46 +0200

    Add recipes for Emacs 25

--- recipes/emacs-25-pre-travis.rb
+++
@@ -1,7 +0,0 @@
-recipe 'emacs-25-pre-travis' do
-  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.8.0/emacs-25-pre-travis.tar.gz'
-
-  install do
-    copy build_path, installations_path
-  end
-end
