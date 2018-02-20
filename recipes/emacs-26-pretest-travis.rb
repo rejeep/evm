1b2374b72e801cde0bb551d907ebcc9a288ce8be
Sam Brightman <sam.brightman@gmail.com>
2018-02-20 20:27:29 +0000

    Update emacs-26-pretest-travis without X11 support
    Fixes #103.

--- recipes/emacs-26-pretest-travis.rb
+++ recipes/emacs-26-pretest-travis.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-26-pretest-travis' do
-  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.12.0/emacs-26-pretest-travis.tar.gz'
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.13.0/emacs-26-pretest-travis.tar.gz'
 
   install do
     copy build_path, installations_path
