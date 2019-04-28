0f4da6ade28e1706744a36d38b08de0902bf1ad8
Sam Brightman <sam.brightman@gmail.com>
2019-04-28 17:22:36 +0100

    Make package list sorted

--- lib/evm/command/list.rb
+++ lib/evm/command/list.rb
@@ -2,7 +2,7 @@ module Evm
   module Command
     class List
       def initialize(*)
-        packages = Evm::Package.all
+        packages = Evm::Package.all.sort{|p1, p2| p1.to_s <=> p2.to_s }
         packages.each do |package|
           if package.current?
             STDOUT.print '* '
