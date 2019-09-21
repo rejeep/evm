45c11effbafb895591af866247dfbd75ca5b1a2d
Sam Brightman <sam.brightman@gmail.com>
2019-09-21 09:30:52 +0100

    Temporarily update URL to point at a real release tarball

--- recipes/emacs-26.3-travis.rb
+++ recipes/emacs-26.3-travis.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-26.3-travis' do
-  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.17.0/emacs-26.3-travis.tar.gz'
+  tar_gz 'https://www.dropbox.com/s/q2rptzjhl3dkx5w/emacs-26.3-travis.tar.gz?dl=1'
 
   install do
     copy build_path, installations_path
