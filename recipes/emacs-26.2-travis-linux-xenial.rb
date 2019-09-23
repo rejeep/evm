0bf9cea5cf186a335e4d017c02d359ecb416af88
Sam Brightman <sam.brightman@gmail.com>
2019-09-23 21:28:55 +0100

    Fix typos

--- recipes/emacs-26.2-travis-linux-xenial.rb
+++ recipes/emacs-26.2-travis-linux-xenial.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-26.2-travis-linux-xenial' do
-  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.18.0/emacs-26.2-travis.tar.gz'
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.18.0/emacs-26.2-travis-linux-xenial.tar.gz'
 
   install do
     copy build_path, installations_path
