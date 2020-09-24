2263bcb6590cd84744de5d64c8eb0355e98826e7
Sam Brightman <sam.brightman@gmail.com>
2020-09-24 21:44:49 +0100

    Update Git snapshots to Emacs 28 development

--- recipes/emacs-git-snapshot-travis-linux-xenial.rb
+++ recipes/emacs-git-snapshot-travis-linux-xenial.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-git-snapshot-travis-linux-xenial' do
-  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.20.0/emacs-git-snapshot-travis-linux-xenial.tar.gz'
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.22.0/emacs-git-snapshot-travis-linux-xenial.tar.gz'
 
   install do
     copy build_path, installations_path
