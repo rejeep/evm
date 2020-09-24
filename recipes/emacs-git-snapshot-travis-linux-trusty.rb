77d41ac2482f34cd5a703b7223114a669575c9e3
Sam Brightman <sam.brightman@gmail.com>
2020-09-24 22:56:01 +0100

    Revert "Temporarily add public URLs for pre-release testing"
    This reverts commit 5a815f5eda6427992546d87fedfbe30d2b1c9fd3.

--- recipes/emacs-git-snapshot-travis-linux-trusty.rb
+++ recipes/emacs-git-snapshot-travis-linux-trusty.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-git-snapshot-travis-linux-trusty' do
-  tar_gz 'https://www.dropbox.com/s/b1jmlm8d8ubxi52/emacs-git-snapshot-travis-linux-trusty.tar.gz?dl=1'
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.22.0/emacs-git-snapshot-travis-linux-trusty.tar.gz'
 
   install do
     copy build_path, installations_path
--- recipes/emacs-git-snapshot-travis-linux-xenial.rb
+++ recipes/emacs-git-snapshot-travis-linux-xenial.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-git-snapshot-travis-linux-xenial' do
-  tar_gz 'https://www.dropbox.com/s/j4t3tp1uxz0rjc9/emacs-git-snapshot-travis-linux-xenial.tar.gz?dl=1'
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.22.0/emacs-git-snapshot-travis-linux-xenial.tar.gz'
 
   install do
     copy build_path, installations_path
