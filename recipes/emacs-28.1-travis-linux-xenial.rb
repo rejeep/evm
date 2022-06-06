14821b4161e5fba150ad6c15eed6f9e68e052c04
Sam Brightman <sam.brightman@gmail.com>
2022-06-07 00:11:07 +0100

    Revert "Temporarily add public URLs for pre-release testing"
    This reverts commit c6b72c7be315c99199b1570844ee838f144aedb7.

--- recipes/emacs-28.1-travis-linux-xenial.rb
+++ recipes/emacs-28.1-travis-linux-xenial.rb
@@ -1,5 +1,5 @@
 recipe 'emacs-28.1-travis-linux-xenial' do
-  tar_gz 'https://www.dropbox.com/s/b055iaypjy2d4ul/emacs-28.1-travis-linux-xenial.tar.gz?dl=1'
+  tar_gz 'https://github.com/rejeep/evm/releases/download/v0.24.0/emacs-28.1-travis-linux-xenial.tar.gz'
 
   install do
     copy build_path, installations_path
